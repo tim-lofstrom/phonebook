dc02f22826cc0c14f0ee5ed7c4b7298fd33b2a29
Tim Löfström <tim.lofstrom.danielsson@lm.se>
2022-05-24 13:49:33 +0200

    update

--- src/phonebook/core.clj
+++ src/phonebook/core.clj
@@ -1,21 +1,30 @@
-(ns phonebook.core :require [clojure.string :as str])
+(ns phonebook.core
+  (:require [clojure.string :as str]))
+
+;; (require '[clojure.tools.namespace.repl :refer [refresh-all refresh]]) (refresh)
+
+(declare actions)
+
+(defn dsa [] "DWS")
 
 (def phonebook-state (atom {}))
 
 (defn help []
   (let [action-names (map name (keys actions))]
-    (doseq [item action-names]
-      (println item))))
+    (doseq [item action-names] (println item))))
 
 (defn add [name number]
   (swap! phonebook-state (fn [pb] (assoc pb name number))))
 
-(def actions {:help help :add add})
+(defn lookup [name]
+  (println (get @phonebook-state name)))
+
+(def actions
+  {:help help :add add :lookup lookup})
 
 (defn handle-input [input]
-(let [args (str/split input #" ")])
-  (let [action (keyword (first args))]
-    ((action actions))))
+  (let [args (str/split input #" ") action (keyword (first args))]
+    (apply (action actions) (rest args))))
 
 (defn phonebook []
   (loop []
